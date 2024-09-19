12447ed6bc6e6ad1987559399b393ad6bf9f3ad0
Elad Kaplan <kaplan.elad@gmail.com>
2024-09-19 14:25:27 +0300

    fix test

--- tests/tasks/list_contacts.rs
+++ tests/tasks/list_contacts.rs
@@ -6,12 +6,12 @@ use serial_test::serial;
 
 #[tokio::test]
 #[serial]
-async fn test_can_run_list_users() {
+async fn test_can_run_list_contacts() {
     let boot = testing::boot_test::<App>().await.unwrap();
 
     assert!(run_task::<App>(
         &boot.app_context,
-        Some(&"list_users".to_string()),
+        Some(&"list_contacts".to_string()),
         &task::Vars::default()
     )
     .await
